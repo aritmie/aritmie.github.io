bd5308f109269fa256cb3981b0cbeca4817d207e
aritmie <44023198+aritmie@users.noreply.github.com>
2019-06-30 15:39:13 +0800

    poc
    load video on click

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+var click_count = 0; 
+
 function swap_colour() {
 
+	click_count+=1;
+	console.log(click_count);
+
 	var main_content = document.getElementById("main_content");
-	main_content.classList.toggle("contentonclick"); 
+
+	if (click_count == 13) {
+
+		window.location.assign("arme001.html");
+
+	} else {
+		main_content.classList.toggle("contentonclick"); 
+	}
+	
 
 }
